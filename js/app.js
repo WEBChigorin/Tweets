5c4780df1ede4585a909457e93b17231ade4b602
MikleMikle <mmchigorin@gmail.com>
2018-09-16 23:57:03 +0300

    Add event listeners to add tweets

---
+++ js/app.js
@@ -0,0 +1,38 @@
+//Variables
+
+const tweetList = document.getElementById('tweet-list');
+
+
+
+//Event listeners
+eventListeners()
+
+function eventListeners() {
+    //form Submission
+    document.querySelector('#form').addEventListener('submit', newTweet);
+}
+
+
+//Functions
+
+function newTweet(event) {
+    event.preventDefault(); // stops the form's default submion
+    
+    //read the text area value
+    const tweet = document.getElementById('tweet').value;
+
+    //create a remove button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet'; //I've already have this class in css
+    removeBtn.textContent = 'X';
+
+    //create li element with text content
+    const li = document.createElement('li'); // now I have li
+    li.textContent = tweet;  // and this li gonna get the value of the textarea
+
+    //add this button to each tweet
+    li.appendChild(removeBtn);
+
+    //Add to the list
+    tweetList.appendChild(li);
+}
